05aa93e0e59e0cdc05fd9b2c4da014e4089856f1
toki-debug <toki.debug@gmail.com>
2021-08-04 15:51:19 -0300

    .sh update

--- apaga.sh
+++ apaga.sh
@@ -1,2 +1,10 @@
 find . -type f -path "./main/*/*" -name "*.class" -type f
+find . -type f -path "./main/*" -name "*.class" -type f
+find . -type f -path "./Server/*" -name "*.class" -type f
+find . -type f -path "./main/*/*" -name "*.class" -delete
+find . -type f -path "./main/*" -name "*.class" -delete
+find . -type f -path "./Server/*" -name "*.class" -delete
+cd Server
+javac Server.java
+java Server
 
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cp src/main/java/* -r bin/main
 cp src/main/resources/* -r bin/main
-cd bin/main
+cp src/Server/* -r bin/Server
+cd bin/main/
 javac Main.java
 java Main
